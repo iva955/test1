dfaeaacd56580cbdf3046a7ee2957d197eab0ff0
iva955 <46595028+iva955@users.noreply.github.com>
2024-01-20 00:20:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function onButtonClick () {
   alert ('Button Clicked'); }
 
+const button = document.querySelector('button');
+button.addEventListener('click', onButtonClick);
+
+const newButton = document.createElement('button');
+newButton.textContent = 'Click me!';
+document.body.appendChild(newButton);
+
+newButton.addEventListener('click', () => {
+  alert('New button clicked!');
+});
